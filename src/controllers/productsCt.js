4649673859e04334cb6dfc8be8b661fae9e4d8c7
Lautaro_Agustín_Bustos <lautaroabustos@gmail.com>
2023-11-28 14:19:41 -0300

    changes that show products on the screen

--- src/controllers/productsCt.js
+++ src/controllers/productsCt.js
@@ -1,1 +1,13 @@
-function getAll (req, res)
+import { getAllProducts } from "../models/productsMD";
+
+async function getAll(req, res) {
+  try {
+    const products = await getAllProducts();
+    res.json(products);
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+    res.status(500).json({ error: "Error al obtener productos" });
+  }
+}
+
+export { getAll };
--- src/routes/productsRt.js
+++ src/routes/productsRt.js
@@ -2,17 +2,24 @@ import { Router } from "express";
 export const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("obteniendo productos");
+  res.json({ message: "Obteniendo todos los productos" });
 });
+
 router.get("/:id", (req, res) => {
-  res.send("obteniendo 1 producto");
+  const productId = req.params.id;
+  res.json({ message: `Obteniendo el producto con ID ${productId}` });
 });
+
 router.post("/", (req, res) => {
-  res.send("creando productos");
+  res.json({ message: "Creando un nuevo producto" });
 });
-router.put("/", (req, res) => {
-  res.send("actualizando productos");
+
+router.put("/:id", (req, res) => {
+  const productId = req.params.id;
+  res.json({ message: `Actualizando el producto con ID ${productId}` });
 });
-router.delete("/", (req, res) => {
-  res.send("eliminando productos");
+
+router.delete("/:id", (req, res) => {
+  const productId = req.params.id;
+  res.json({ message: `Eliminando el producto con ID ${productId}` });
 });
